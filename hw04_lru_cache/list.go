538b8e8188516ae6263dc0ecfb47181756223666
shumakovma <ms.profile.dev@gmail.com>
2022-03-10 13:43:02 +0500

    hw04_lru_cache: Фиксация решения.

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,112 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	Length    int
+	FrontItem *ListItem
+	BackItem  *ListItem
+}
+
+func (l *list) Len() int {
+	return l.Length
+}
+
+func (l *list) Front() *ListItem {
+	return l.FrontItem
+}
+
+func (l *list) Back() *ListItem {
+	return l.BackItem
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newItem := &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  nil,
+	}
+
+	if l.FrontItem != nil {
+		l.FrontItem.Prev = newItem
+		newItem.Next = l.FrontItem
+	}
+
+	l.FrontItem = newItem
+
+	if l.BackItem == nil {
+		l.BackItem = newItem
+	}
+
+	l.Length++
+
+	return l.FrontItem
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newItem := &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  nil,
+	}
+
+	if l.BackItem != nil {
+		l.BackItem.Next = newItem
+		newItem.Prev = l.BackItem
+	}
+
+	l.BackItem = newItem
+
+	if l.FrontItem == nil {
+		l.FrontItem = newItem
+	}
+
+	l.Length++
+
+	return l.BackItem
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i == nil {
+		return
+	}
+
+	if l.FrontItem == i {
+		l.FrontItem = i.Next
+	}
+
+	if l.BackItem == i {
+		l.BackItem = i.Prev
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+
+	l.Length--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == nil {
+		return
+	}
+
+	l.Remove(i)
+
+	if l.FrontItem != nil {
+		l.FrontItem.Prev = i
+		i.Next = l.FrontItem
+	}
+
+	l.FrontItem = i
+
+	if l.BackItem == nil {
+		l.BackItem = i
+	}
+
+	l.Length++
 }
 
 func NewList() List {
